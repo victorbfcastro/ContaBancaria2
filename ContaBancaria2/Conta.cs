0b5528c118605c2ec1670c6b8431f2d0fc2065ce
chrisAndriotti <andriottichris@gmail.com>
2020-12-11 12:05:16 -0300

    Adicionar arquivos de projeto.

---
+++ ContaBancaria2/Conta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ContaBancaria2
+{
+    class Conta
+    {
+        public int Numero { get; set; }
+        public string Titular { get; set; }
+        public double Saldo { get; set; }
+        public double LimiteDeSaque { get; set; }
+
+        public Conta()
+        {
+
+        }
+        public Conta(int numero, string titular, double saldo)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+
+    }
+}
---
+++ ContaBancaria2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conta = CriarConta();
+
+        }
+        public static Conta CriarConta()
+        {
+            Console.Write("Entre com o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre com o nome do titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Entre com o saldo inicial: ");
+            double saldo = double.Parse(Console.ReadLine());
+
+            return new Conta(numero, titular, saldo);
+        }
+    }
+}
